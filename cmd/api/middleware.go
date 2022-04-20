ab10d4fb96ca018f6495244635651246f0347ead
Jere Penttinen <jere@disroot.org>
2022-04-20 08:51:17 +0300

    Fix publication nav

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -17,6 +17,7 @@ func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
 		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
 
 		next.ServeHTTP(w, r)
 	})
